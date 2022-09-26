fbed5153ec8eb90b5330b3b25e3d318dc01266e2
nadssnchez <nadiarossario@gmail.com>
2022-09-25 23:52:50 -0300

    Retoque 2. Nadia Rosario Sanchez

--- src/components/Integrantes.js
+++ src/components/Integrantes.js
@@ -3,7 +3,7 @@ import iconGit from '../assets/icons/icon-github.png'
 function Integrantes(props){
     return(
         <div className="profile">
-            <div className="gitname">{props.nombre}</div>
+            <div className="gitname">{props.gitname}</div>
             <div className="fullname">Alumno: {props.fullname}</div>
             <img className="imagen" src={iconGit}/>
         </div>
