70bc22bf756526110c0ca9938d026d4acba58b2d
Leonel2099 <ezequielleonel1@gmail.com>
2022-09-25 23:21:12 -0300

    Ultima modificacion en el componente Game y uso de componentes de react-bootstrap Choque Leonel

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import img  from '../assets/icons/logo.png';
+export default function NavBar() {
+    return (
+        <div className='container-nav'>
+            <Navbar variant="dark">
+                <Container className='mx-3'>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt="image-nav"
+                            src={img}
+                            width="60"
+                            height="60"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/PlayGame">PlayGame</Nav.Link>
+                        <Nav.Link href="/AboutPage">AboutPage</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+
+    )
+}
